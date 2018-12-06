89d0a1c83ea9647bd0d3dc1ebcee8a47efdb6ce1
Caio Oliveira <caioaao@gmail.com>
2018-12-06 15:58:23 -0500

    remove speclj and bump clojure to use core.spec

--- clojure/project.clj
+++ clojure/project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:dependencies [[speclj "3.3.2"]]}}
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :profiles {:dev {:dependencies [[nubank/matcher-combinators "0.4.2"]
+                                  [org.clojure/test.check "0.10.0-alpha3"]]}}
   :plugins [[speclj "3.3.2"]]
   :test-paths ["spec"])
