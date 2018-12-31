c5fa2eb2f56907fe76c40db66527b30fb63d5b12
Caio Oliveira <caioaao@gmail.com>
2018-12-31 15:09:36 -0500

    better tooling

--- clojure/project.clj
+++ clojure/project.clj
@@ -3,7 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :profiles {:dev {:dependencies [[speclj "3.3.2"]]}}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/spec.alpha "0.2.176"]]
+  :profiles {:dev  {:resource-paths ["resources/dev"]
+                    :dependencies   [[nubank/matcher-combinators "0.4.2"]
+                                     [org.clojure/test.check "0.10.0-alpha3"]
+                                     [org.clojure/tools.namespace "0.2.11"]]}
+             :test {:resource-paths ["resources/test"]}}
   :plugins [[speclj "3.3.2"]]
   :test-paths ["spec"])
